0c1c6c270d15b6780b441aaa79a8b5693bbef688
RupeshBhulode <131619196+RupeshBhulode@users.noreply.github.com>
2026-03-11 11:10:57 +0530

    Update OrderController.java

--- demo/src/main/java/cash/demo/controller/OrderController.java
+++ demo/src/main/java/cash/demo/controller/OrderController.java
@@ -1,13 +1,12 @@
 package cash.demo.controller;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import cash.demo.entity.Order;
 import cash.demo.service.CashfreeService;
 import cash.demo.service.OrderService;
+import jakarta.servlet.http.HttpServletRequest;
 import java.util.Map;
 
 @RestController
@@ -18,35 +17,36 @@ public class OrderController {
     private OrderService orderService;
 
     @Autowired 
-    public CashfreeService cachfreeService;
+    private CashfreeService cashfreeService;
 
     @PostMapping("/create")
-    public ResponseEntity<Map<String, Object>>  createOrder(@RequestBody Order order){
+    public ResponseEntity<Map<String, Object>> createOrder(
+            @RequestBody Order order,
+            HttpServletRequest servletRequest) {
+
         order.setId(null);
-         Order createdOrder = orderService.createOrder(order);
 
+        // Dynamically get base URL from incoming request
+        String baseUrl = servletRequest.getScheme() + "://" + 
+                         servletRequest.getServerName();
 
-        Map<String,Object> paymentSession= cachfreeService.createPaymentSession(
+        Order createdOrder = orderService.createOrder(order);
 
-              createdOrder.getOrderId(),
-              createdOrder.getAmount(),
-              createdOrder.getCustomerName(),
-              createdOrder.getCustomerEmail(),
-              createdOrder.getCustomerPhone()
+        Map<String, Object> paymentSession = cashfreeService.createPaymentSession(
+            createdOrder.getOrderId(),
+            createdOrder.getAmount(),
+            createdOrder.getCustomerName(),
+            createdOrder.getCustomerEmail(),
+            createdOrder.getCustomerPhone(),
+            baseUrl
         );
 
-
-
-
-         return ResponseEntity.ok(paymentSession);
+        return ResponseEntity.ok(paymentSession);
     }
 
-
-
     @GetMapping("/{orderId}")
-    public ResponseEntity<Order> getOrder(@PathVariable String orderId){
+    public ResponseEntity<Order> getOrder(@PathVariable String orderId) {
         Order order = orderService.getOrderByOrderId(orderId);
         return ResponseEntity.ok(order);
     }
-    
 }
